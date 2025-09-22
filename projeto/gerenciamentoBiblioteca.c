376e5ebe1e324a4b71e439f75aac50b351423257
Elton Gustavo da Silva Freitas <dev.eltongustavo@gmail.com>
2025-09-22 18:29:46 -0300

    Estrutura do Main do projeto da biblioteca

--- projeto/gerenciamentoBiblioteca.c
+++ projeto/gerenciamentoBiblioteca.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
 
 int main() {
-    
+    int opcao;
+    while (1) {
+        printf("SISTEMA DA BIBLIOTECA\n\n");
+        printf("1. Cadastrar Livros\n");
+        printf("2. Alugar Livros\n");
+        printf("3. Listar Todos os Livros\n");
+        printf("4. Sair\n");
+
+        printf("\nSelecione uma das opções acima (1, 2, 3, 4): ");
+        scanf("%d", &opcao);
+
+        if (opcao == 1) {
+            /* code */ 
+        }
+        else if (opcao == 2) {
+            /* code */
+        }
+        else if (opcao == 3) {
+            /* code */
+        }
+        else if (opcao == 4) {
+            break;
+        }
+        else {
+            printf("Opção Inválida!");
+        }      
+        
+    }
+
+    printf("Sistema encerrado!\n");
 }
